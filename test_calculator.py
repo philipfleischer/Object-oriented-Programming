5132c228a8ceb243c39f965cfe613a93700230b6
philipef <philipef@uio.no>
2025-08-23 17:15:25 +0200

    Well this did not work. Read that the isClose function might work

--- test_calculator.py
+++ test_calculator.py
@@ -1,13 +1,15 @@
 from calculator import add
+import math
 
 def test_add_ex1():
     res = add(1, 2)
     assert res == 3, f"Expected the sum to be 3, not {res}" 
     
-test_add_ex1()
+
 
 def test_add_ex2():
-    res = add(0.1, 0.2)
-    assert res == 0.3, f"Expected the sum to be 3, not {res}" 
+    val1 = 0.1
+    val2 = 0.2
+    res = abs(add(val1, val2))
+    assert abs(res) == 0.3, f"Expected the sum to be 0.3, not {res}" 
     
-test_add_ex2()
