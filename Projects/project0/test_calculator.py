484e67b8707ee1c2328e05753ea7780b4b6e29ca
philipef <philipef@uio.no>
2025-08-23 17:06:39 +0200

    Made the second test, seems like the float numbers are not the exact values i wrote, so I think it will be solved by using the absolute values of the values and sum

--- Projects/project0/test_calculator.py
+++ Projects/project0/test_calculator.py
@@ -1,8 +1,13 @@
 from calculator import add
 
-def test_add():
+def test_add_ex1():
     res = add(1, 2)
     assert res == 3, f"Expected the sum to be 3, not {res}" 
     
+test_add_ex1()
 
-test_add()
+def test_add_ex2():
+    res = add(0.1, 0.2)
+    assert res == 0.3, f"Expected the sum to be 3, not {res}" 
+    
+test_add_ex2()
