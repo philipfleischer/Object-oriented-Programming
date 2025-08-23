cf0e33fe09bbfba3ea6e7dc8417da68f418d7a14
philipef <philipef@uio.no>
2025-08-23 16:48:42 +0200

    Added the docstring in the add function, and have now a completely working code and test for excercise-3.

--- calculator.py
+++ calculator.py
@@ -1,2 +1,3 @@
 def add(x: float, y: float) -> float:
+    """Returns the sum of float number x and float number y """
     return x + y
--- test_calculator.py
+++ test_calculator.py
@@ -3,6 +3,6 @@ from calculator import add
 def test_add():
     res = add(1, 2)
     assert res == 3, f"Expected the sum to be 3, not {res}" 
-    assert None == 3
+    
 
 test_add()
