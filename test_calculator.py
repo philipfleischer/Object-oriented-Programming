7014bf812c48a77f04d8c4285c301d64cc978415
philipef <philipef@uio.no>
2025-08-24 08:42:30 +0200

    I added comments to explain the test cases. I removed one of the tests from branch: exercise-5 (test_add_ex2()) and I also renamed test_add_ex1(), since i could merge them into one test function: test_add(val1, val2, expected). Using the parametrized test with three test cases, (two of which where the same as the scenarios in exercise-5), I completed the 6th exercise, and all the tests passed the pytest call in terminal. I hope this was okay to do, I will ask in the plenary session to be safe anyway

--- test_calculator.py
+++ test_calculator.py
@@ -2,10 +2,11 @@ from calculator import add
 import pytest
 
 @pytest.mark.parametrize("val1, val2, expected", [
-    (1, 2, 3),
-    (-1, 1, 0),
-    (0.1, 0.2, 0.3)
+    (1, 2, 3),          #The same test case as test_add_ex1() in branch: exercise5
+    (-1, 1, 0),         #Test to check for negative numbers
+    (0.1, 0.2, 0.3)     #The same test case as test_add_ex2() in branch: exercise5
 ])
+
 def test_add(val1, val2, expected):
     res = add(val1, val2)
     assert res == pytest.approx(expected)
