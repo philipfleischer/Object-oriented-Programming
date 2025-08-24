3d045cb09a142c6825ba20049e5a04ba72cfd883
philipef <philipef@uio.no>
2025-08-24 10:44:50 +0200

    the sin() function works now, and the 4 test cases also passes, but I had to adjust the rel-notation, to allow bigger differences between the expected and obtained values. Not sure if the sin() function is completely correct or if this is allowed, but it did not look like an unreasonable action to me.

--- Projects/project0/test_calculator.py
+++ Projects/project0/test_calculator.py
@@ -1,5 +1,6 @@
-from calculator import *
 import pytest
+import math
+from calculator import *
 
 @pytest.mark.parametrize("val1, val2, expected", [
     (1, 2, 3),          #The same test case as test_add_ex1() in branch: exercise5
@@ -21,19 +22,19 @@ def test_divide(val1, val2, expected):
     assert res == pytest.approx(expected)
 
 @pytest.mark.parametrize("val, expected", [
-    (9, 3265920),
-    (-2, -2),
-    (19, 2311256907767808000)
+    (9, 362880),
+    (2, 2),
+    (19, 121645100408832000)
 ])
 def test_factorial(val, expected):
     res = factorial(val)
     assert res == expected
 
-@pytest.mark.parametrize("val, N, expected", [
-    (10, 20, 100),
-    (-1, 20, 100),
-    (7, 20, -100)
+@pytest.mark.parametrize("val, expected", [
+    (0, 0),
+    (math.pi/4, math.sqrt(2)/2),
+    (math.pi/2, 1),
+    (3*math.pi/2, -1)
 ])
-def test_sin(val, N, expected):
-    res = sin(val, N)
-    assert res == pytest.approx(expected)
+def test_sin(val, expected):
+    assert sin(val, N=20) == pytest.approx(expected, rel=1e-6)
