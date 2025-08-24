02e264e0ec733d4f67d260515e26aa9a4a1b1844
philipef <philipef@uio.no>
2025-08-24 08:55:06 +0200

    Made the empty functions for the calculator, so that TDD is done correctly

--- calculator.py
+++ calculator.py
@@ -1,3 +1,14 @@
 def add(x: float, y: float) -> float:
     """Returns the sum of float number x and float number y """
     return x + y
+
+def divide(x: float, y: float) -> float:
+    """Returns the dividend of two numbers"""
+    pass
+
+def factorial(x: int) -> int:
+    """Returns n! of input: n"""
+    pass
+
+def sin(x: int, N: 20) -> int:
+    pass
