174208ec62a2e4dfa706c097f482d3f06b103a3b
philipef <philipef@uio.no>
2025-08-24 13:16:35 +0200

    I have made the last changes to the working code for exercise 8. All tests pass and I cannot see anything missing for the completion of the assignment. I will wait until the lectures catches up to double check

--- calculator.py
+++ calculator.py
@@ -5,12 +5,14 @@ def add(x: float, y: float) -> float:
     return x + y
 
 def divide(x: float, y: float) -> float:
-    """Returns the dividend of two numbers"""
-    return x//y
+    """Returns the divided result of two float values.
+    Raises ZeroDivisionError if detected"""
+    return x/y
 
 def factorial(x: int) -> int:
-    """Returns n! of input: n, for non-negative int´s"""
-    if type(x) != int: 
+    """Returns n! of input: n, for non-negative integers.
+    Raises Typerror and ValueError if it detects it."""
+    if not isinstance(x, int): 
         raise TypeError("Factorial needs a number as argument")
     if x < 0:
         raise ValueError("Negative not allowed in factorial")
@@ -23,7 +25,8 @@ def factorial(x: int) -> int:
 def sin(x: float, N=20) -> float:
     """
     Approximate sin(x) using Taylor series with N terms.
-    Uses factorial() function above."""
+    Uses factorial() function above.
+    N should be small (e.g.: N=20)"""
     x = ((x + math.pi) % (2*math.pi)) - math.pi
     sinValue = 0
     for i in range(N + 1):
