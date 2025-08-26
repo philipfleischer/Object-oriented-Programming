89a547e5b6c09cb56b9d311d9d512f6e6e0a9b7e
philipef <philipef@uio.no>
2025-08-26 10:56:53 +0200

    I saw that the exercise-6 assignment text said that I needed both the test functions and three test cases each. Right below that, it said we were allowed to overwrite the functions, so I misunderstood and merged the two test funcitons that were almost alike into one test. I only had one function and forgot that I also needed to have 3 test cases per function. I saw that now that I am done with the Project. I talked about this with the Group Teacher in the group session  the 26.aug, and she said this was an acceptable approach to the my conundrum. So I wrote an extra test and parametrized it with 3 new test cases. I also added two assert statements to check for type and value of the parameters in the test cases.

--- Projects/project0/test_calculator.py
+++ Projects/project0/test_calculator.py
@@ -1,5 +1,5 @@
-from calculator import add
 import pytest
+import calculator as calc
 
 @pytest.mark.parametrize("val1, val2, expected", [
     (1, 2, 3),          #The same test case as test_add_ex1() in branch: exercise5
@@ -7,7 +7,19 @@ import pytest
     (0.1, 0.2, 0.3)     #The same test case as test_add_ex2() in branch: exercise5
 ])
 
-def test_add(val1, val2, expected):
-    res = add(val1, val2)
+def test_add_ex1(val1: float, val2: float, expected: float)-> None:
+    res = calc.add(val1, val2)
     assert res == pytest.approx(expected)
-   
+
+@pytest.mark.parametrize("val1, val2, expected", [
+    (5, 7, 12),         #Positive numbers
+    (-3, -4, -7),       #Negative numbers
+    (1000, 0, 1000)     #Using 0
+])
+def test_add_ex2(val1: float, val2: float, expected: float) -> None:
+    res = calc.add(val1, val2)
+    assert res == pytest.approx(expected)
+    #Testing type
+    assert isinstance(res, (int, float))
+    #Testing value
+    assert res >= min(val1, val2) or res <= max(val1, val2)
