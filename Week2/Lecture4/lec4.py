9bba4c5b15aa08c5a18ac786acff747a15672af4
philipfleischer <fleischer1@live.no>
2025-08-29 12:45:34 +0200

    Implemented vector_addition function

--- Week2/Lecture4/lec4.py
+++ Week2/Lecture4/lec4.py
@@ -3,4 +3,7 @@ def vektor_addisjon(u: list[int | float],
                     v: list[int | float]) \
                     -> list[int | float]:
         """Tolker to lister som 3D-vektorer og adderer dem elementvis."""
-        pass
+        w = []
+        for i in range(3):
+            w.append(u[i] + v[i])
+        return w
