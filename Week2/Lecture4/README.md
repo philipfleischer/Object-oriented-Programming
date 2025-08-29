e93673f914c93353b4b40383991a92b3da9f89d9
philipfleischer <fleischer1@live.no>
2025-08-29 12:33:53 +0200

    Made a function to testing

--- Week2/Lecture4/README.md
+++ Week2/Lecture4/README.md
@@ -2,3 +2,4 @@ Notes from Lecture 4 IN1910 fredag 29/8
 
 Testing
 
+nyttige verktøy: assert, exceptions, pytest, TDD, exit-koder, 
---
+++ Week2/Lecture4/lec4.py
@@ -0,0 +1,6 @@
+# \ betyr å fortsette på neste linje
+def vektor_addisjon(u: list[int | float], 
+                    v: list[int | float]) \
+                    -> list[int | float]:
+        """Tolker to lister som 3D-vektorer og adderer dem elementvis."""
+        pass
