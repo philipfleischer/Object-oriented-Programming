8b361081ef346a301594d3910ff5fc21541543ce
philipfleischer <fleischer1@live.no>
2025-09-08 09:34:27 +0200

    Implemented ExponentialDecay.setter and property for decay variable self._a

--- exp_decay.py
+++ exp_decay.py
@@ -18,3 +18,21 @@ class ExponentialDecay:
         og du/dt vil ha like mange elementer).)"""
         du_dt = -self._a * u
         return du_dt
+
+    #With this, the decay works as a variable instead of a function
+    @property
+    def decay(self) -> float:
+        """Returnerer decay-konstanten a."""
+        return self._a
+
+    @decay.setter #Run on writing it in code?
+    def decay(self, value: float) -> None:
+        """Endrer decay-konstanten a < ValueError hvis ikke positiv verdi"""
+        if value < 0:
+            raise ValueError(f"ExponentialDecay.__init__: value er negativ ({value})")
+        self._a = value
+
+#Eksempel-kode (kjøres ikke ved import)
+if __name__ == "__main__":
+    eksempel = ExponentialDecay(2.0)
+    print(eksempel.decay)
