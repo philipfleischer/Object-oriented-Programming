e2e52347fcda39ae0cc72839af9fe9c3970ed0e2
philipfleischer <fleischer1@live.no>
2025-09-08 10:01:53 +0200

    Defined ODEModel with __call__

--- Projects/project1/exp_decay.py
+++ Projects/project1/exp_decay.py
@@ -7,16 +7,14 @@ class ExponentialDecay:
     def __init__(self, a: float):
         """a er decay-konstanten, kan ikke være negativ (da får mann
         ValueError)."""
-        if a < 0:
-            raise ValueError(f"ExponentialDecay.__init__: a er negativ ({a})")
-        self._a = a
+        self.decay = a
 
 
     def __call__(self, t: float, u: np.ndarray[float]) ->np.ndarray[float]:
         """Når vi kallet et objekt som en funksjon regnes du/dt ut for 
         disse verdiene av t og u (u kan være en array med mange elementer,
         og du/dt vil ha like mange elementer).)"""
-        du_dt = -self._a * u
+        du_dt = -self.decay * u
         return du_dt
 
     #With this, the decay works as a variable instead of a function
