28f95970eedd1c8f36d233d04906f2b8917fa221
philipfleischer <fleischer1@live.no>
2025-09-08 09:03:09 +0200

    Implemented ExpoenentialDecay.__call__. Mcgyver solution on test of exception (should be improved).

--- exp_decay.py
+++ exp_decay.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class ExponentialDecay:
     """Representerer en difflikning (ODE) på formen:
     du/dt = -au"""
@@ -10,3 +12,9 @@ class ExponentialDecay:
         self._a = a
 
 
+    def __call__(self, t: float, u: np.ndarray[float]) ->np.ndarray[float]:
+        """Når vi kallet et objekt som en funksjon regnes du/dt ut for 
+        disse verdiene av t og u (u kan være en array med mange elementer,
+        og du/dt vil ha like mange elementer).)"""
+        du_dt = -self._a * u
+        return du_dt
