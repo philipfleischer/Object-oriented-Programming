37c246f62a91aa85b4747b4c233244ac20a9acdf
philipfleischer <fleischer1@live.no>
2025-09-22 07:52:27 +0200

    Task 1g is now completed. I made two tests for the solve function; test_solve_time that asserts wether the first element (time point) is zero, if the last time point is T and lastly that the second time point subtracted the first time point equals to dt. The second test function is also implemented with pytest.mark.parametrize using the TEST_CASES i made. This function checks that the relative error rate is equal to or lower than 1%.

--- test_exp_decay.py
+++ test_exp_decay.py
@@ -3,6 +3,13 @@ import pytest
 from exp_decay import ExponentialDecay
 from ode import InvalidInitialConditionError
 
+#Cases for testing: (a, u0_scalar, T, dt)
+TEST_CASES = [
+    (0.4, 3.2, 10.0, 0.01),
+    (1.0, 2.0, 2.0, 0.05),
+    (0.2, 1.5, 5.0, 0.10)
+]
+
 #Test right hand side = test_rhs
 def test_rhs() -> None:
     """
@@ -54,3 +61,47 @@ def test_solve_with_different_number_of_initial_states() -> None:
     assert result.time.ndim == 1
     #Check if the state variable u has one state
     assert result.solution.shape[0] == 1
+
+@pytest.mark.parametrize("a,u0_s,T,dt", TEST_CASES)
+def test_solve_time(a: float, u0_s: float, T: float, dt: float) -> None:
+    """
+    Check that time grid starts at 0, ends at T, and steps by dt.
+    """
+    model = ExponentialDecay(a)
+    u0 = np.array([u0_s], dtype=float)
+
+    result = model.solve(u0, T=T, dt=dt)
+    result_time = result.time
+
+    #Sanity checks for dimensions and length
+    assert result_time.ndim == 1
+    assert len(result_time) >= 2
+
+    #Assert the checkpoints exactly and also the steps
+    #The first time point is zero
+    assert result_time[0] == pytest.approx(0.0, rel=0, abs=1e-12)
+    #The last time point is T
+    assert result_time[-1] == pytest.approx(T, rel=0, abs=1e-12)
+    #The difference between the second and first time point is dt.
+    assert (result_time[1] - result_time[0]) == pytest.approx(dt, rel=0, abs=1e-12)
+
+@pytest.mark.parametrize("a,u0_s,T,dt", TEST_CASES)
+def test_solve_solution(a: float, u0_s: float, T: float, dt: float) -> None:
+    model = ExponentialDecay(a)
+    #The initialized 1-element np array
+    u0 = np.array([u0_s], dtype=float)
+
+    #Solving the ODE with end time T and step dt
+    result = model.solve(u0, T=T, dt=dt)
+    #Extracting the time points
+    result_time = result.time
+    #result.solution = (num_states, num_time_points)
+    #   result.solution[0] = num_states
+    y = result.solution[0]
+    #We compute the exact solution at the given time point
+    y_exact = u0_s * np.exp(-a*result_time)
+
+    #Computing the rel_err between num and exact solution
+    relative_error = np.linalg.norm(y - y_exact) / np.linalg.norm(y_exact)
+    #Checking for 1% or less difference
+    assert relative_error <= 0.01
