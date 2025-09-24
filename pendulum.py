ef341144060815faad8621c0f1e79e54703bedff
philipfleischer <fleischer1@live.no>
2025-09-24 08:21:16 +0200

    Task 2c complete. Implemented PendulumResults dataclass and override the function _create_result. Added properties to extract omeage and theta values from PendulumResults dataclass. Overrode the _create_result() function to return a more specific ODE, instead of the generic ODEResult, we originally had and used. Added docstrings and comments for readability.

--- pendulum.py
+++ pendulum.py
@@ -1,8 +1,38 @@
 import numpy as np
-from typing import Final
+from typing import Final, Any
+from dataclasses import dataclass
 from ode import *
 
 DEFAULT_G: Final[float] = 9.81
+
+@dataclass
+class PendulumResults:
+    """Results from solving the pendulum problem.
+    
+    Args:
+        time (np.ndarray): The timesteps of the solution.
+        solution (np.ndarray): The values of the solution at the given timesteps.
+        L (float): The length of the pendulum rod.
+        g (float): The gravitational acceleration.
+    """
+    time: np.ndarray
+    solution: np.ndarray
+    L: float
+    g: float
+
+    @property
+    def theta(self) -> np.ndarray:
+        """
+        The angular displacement over time.
+        """
+        return self.solution[0]
+
+    @property
+    def omega(self) -> np.ndarray:
+        """
+        The angular velocity over time.
+        """
+        return self.solution[1]
 
 class Pendulum(ODEModel):
     """
@@ -19,9 +49,9 @@ class Pendulum(ODEModel):
     g:  float
         Gravitational accelaration (m/s^2), default 9.81.
     """
-    def __init__(self, *, L: float=1.0, g: float=DEFAULT_G):
-        if L <= 0: raise ValueError("Pendullum length L must be a positivie integer.")
-        if g < 0: raise ValueError("Gravitioanl acceleration g must be positive.")
+    def __init__(self, *, L: float=1.0, g: float=DEFAULT_G) -> None:
+        if L <= 0: raise ValueError("Pendulum length L must be a positivie integer.")
+        if g < 0: raise ValueError("Gravitational acceleration g must be positive.")
         self._L = float(L)
         self._g = float(g)
     
@@ -72,3 +102,60 @@ class Pendulum(ODEModel):
         #The derivative of omega with respect to time
         domega_dt = -(self.g / self.L) * np.sin(theta)
         return np.array([dtheta_dt, domega_dt], dtype=float)
+    
+    def _create_result(self, solution: Any) -> PendulumResults:
+        """
+        This method converts the raw numerical solution from SciPy into a
+        PendulumResults object.
+        
+        The SciPy function solve_ivp returns a complicated object, since we
+        only need time points and solution values, we use _create_result()
+        to extract fields and combine them wit L and g, thereby making a 
+        simpler and defined data class to store the data in.
+
+        When returning PendulumResults object, it is tied to the current
+        Pendulum object. This results in us having a fast and efficient 
+        way of accessing theta and omega, and also to the time points and
+        solution points.
+
+        Parameters:
+        solution: Any
+            The result from scipy.integrate.solve_ivp:
+                t: np.array of time points
+                y: np.array of solution values
+        
+        Return:
+            PendulumResults: time, solution and pendulum params
+        
+        Raises:
+            AttributeError: If .t and .y is not correct in Pendulum object
+        """
+        if not hasattr(solution, "t") or not hasattr(solution, "y"):
+            raise AttributeError("Solution object must have attributes t and y.")
+        return PendulumResults(
+            time=solution.t, solution=solution.y, L=self.L, g=self.g
+        )
+
+def exercise_2b() -> ODEResult:
+    """
+    Create a Pendulum, solve with u0=[pi/6], T=10, dt=0.01, ans save plot.
+    """
+    model = Pendulum(L=1.0, g=9.81)
+
+    u0 = np.array([np.pi / 6.0, 0.35], dtype=float) #[Tetha, Omega]
+    T = 10.0
+    dt = 0.01
+
+    result = model.solve(u0=u0, T=T, dt=dt)
+
+    #Latex
+    state_labels = [r"$\theta$", r"$\omega$"]
+    plot_ode_solution(results=result, state_labels=state_labels, filename="exercise_2b.png")
+    return result
+
+if __name__ == "__main__":
+    model = Pendulum(L=1.42, g=9.81)
+    u0 = np.array([np.pi/6, 0.35])
+    result = model.solve(u0=u0, T=10, dt=0.01)
+    print(isinstance(result, PendulumResults))
+    exercise_2b()
