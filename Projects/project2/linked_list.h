33b1144c579367ced88c226a47d257430adb1ab6
philipfleischer <fleischer1@live.no>
2025-10-18 15:32:14 +0200

    Task 2a completed. Implemented doubly linked list data structure. - Converted the Node class from a singly linked list to be a doubly linked list data structure. The dll (doubly linked list) has a prev pointer to the previous node in addition to the next node pointer. - Added a _tail pointer to be able to locate the last element in the dll, enables fast appends in O(1) time complexity. - Updated the constructor and destructor of the singly linked list to be a dll that can handle both a head and tail pointer. - Edited the push_front() methid to be able to update both pointers when the list is empty and not empty. - Made sure test_empty_list_has_zero_length() worked as expected and made a new test function test_push_front() that tested the dll push_front function. - Added and edited documentation in doxygen documentation style for the three files. All tests passed

--- Projects/project2/linked_list.h
+++ Projects/project2/linked_list.h
@@ -1,66 +1,115 @@
+#pragma once
+#include <iostream>
+#include <stdexcept>
+
 /**
- * A node in a singly linked list.
- * 
- * Not intended for direct use, but used my the LinkedList class.
+ * @brief Node in a doubly linked list.
+ *
+ * @details
+ * Holds an integer value and links to the previous and next nodes.
+ * This type is an implementation detail of @ref LinkedList and is not intended for direct use by client code.
+ *
  * @see LinkedList
  */
 struct Node
 {
-    /// Value of an element in the list (default: most negative 32-bit int)
+    /**
+     * @brief Stored value for the node.
+     *
+     * @note Value of an element in the list (default: most negative 32-bit int). Real values are set by the linked list.
+     */
     int value = -2147483647;
 
-    /// Pointer to the next element in the list (default: null pointer)
-    Node* next = nullptr;
+    /**
+     * @brief Pointer to the next node in the list.
+     *
+     * @note nullptr if this is the tail (or the list is empty).
+     */
+    Node *next = nullptr;
+
+    /**
+     * @brief Pointer to the previous node in the list.
+     *
+     * @note nullptr if this is the head (or the list is empty).
+     */
+    Node *prev = nullptr;
 };
 
 /**
- * A singly linked list that supports indexing.
+ * @brief A doubly linmked list.
+ *
+ * @details
+ * Supports push to front, append to back, printing, and length queries.
+ * The list maintains pointers to both head and tail to make front/back operations efficient.
+ *
+ * @invariant
+ * - If the list is empty, that means: _head == nullptr && _tail == nullptr && _size == nullptr.
+ * - If the list is not empty, then: _head->next != nullptr && _tail->prev != nullptr.
+ * - _size is equal to the number of nodes counted from _head->next until it equals nullptr.
  */
 class LinkedList
 {
-    private:
-        /// @internal Pointer to the first element in the list
-        Node* _head;
+private:
+    /// @internal Pointer to the first element in the list (head), nullptr on empty
+    Node *_head;
 
-        /**
-         * @internal Tracks the number of elements in the list
-         * @see length
-         */ 
-        int _size;
+    /// @internal Pointer to the last element in the list (tail), nullptr on empty
+    Node *_tail;
 
-        /**
-         * Check wheter the given index if out of
-         * bound and throw a range error if it is
-         *
-         * @param index The index to be checked
-         * @throws std::range_error Thrown if the index is negative or
-         *         outside the list
-         */
-        void _check_index_out_of_bounds(int index);
+    /**
+     * @internal Tracks the number of elements in the list.
+     * @see length().
+     */
+    int _size;
 
-    public:
-        LinkedList();
-        ~LinkedList();
+    /**
+     * Check wheter the given index is out of bounds and throw a range error if it is.
+     *
+     * @param index The index to be checked
+     * @throws std::range_error Thrown if the index is negative or
+     *         outside the list
+     */
+    void _check_index_out_of_bounds(int index);
 
-        /**
-         * Adds a new integer element to the end of the list.
-         * 
-         * @param val The value of the element
-         */
-        void append(int val);
+public:
+    /**
+     * @brief Constructs an empty list.
+     *
+     * @post length == 0, _head == nullptr, _tail == nullptr.
+     */
+    LinkedList();
 
-        /// @return The number of elements in the list
-        int length();
+    /**
+     * @brief Destroys the list and releases all nodes.
+     *
+     * @post All dynamically allocated nodes are deleted.
+     */
+    ~LinkedList();
 
-        /**
-         * Print values in the list
-         */
-        void print();
+    /**
+     * @brief Appends a value at the end of the list.
+     *
+     * @param val The value of the element to append.
+     * @post length increments by 1 and new value is at index length-1
+     */
+    void append(int val);
 
-        /**
-         * Add element to the beginning of the list
-         *
-         * @param val The value to be added
-         */
-        void push_front(int val);
+    /// @return The number of elements in the list.
+    int length();
+
+    /**
+     * @brief Prints the list contents.
+     *
+     * @details
+     * The format is [a0, a1, ..., an].
+     */
+    void print();
+
+    /**
+     * @brief Inserts a value at the front of the list.
+     *
+     * @param val The value to be pushed at the head.
+     * @post length increments by 1 and new value at index 0.
+     */
+    void push_front(int val);
 };
--- Projects/project2/test_linked_list.cpp
+++ Projects/project2/test_linked_list.cpp
@@ -1,18 +1,36 @@
 #include <cassert>
 #include <iostream>
 
-#include "linked_list.cpp"
+#include "linked_list.h"
 
 void test_empty_list_has_zero_length()
 {
     LinkedList ll = LinkedList();
     std::cout << "Test that empty list has length zero";
     assert(ll.length() == 0);
-    std::cout << " - Success!" << std::endl;;
+    std::cout << " - Success!" << std::endl;
+    ;
+}
+
+void test_push_front()
+{
+    std::cout << "Test push_front.\n";
+    LinkedList ll;
+    ll.push_front(3);
+    ll.push_front(2);
+    ll.push_front(1);
+    assert(ll.length() == 3);
+
+    std::cout << "- Expected: \n[1, 2, 3]\nResult: "
+              << std::endl;
+    ll.print();
+    std::cout << "- Success: test_push_front()\n"
+              << std::endl;
 }
 
 int main()
 {
     test_empty_list_has_zero_length();
+    test_push_front();
     return 0;
 }
