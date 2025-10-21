b6818f6f49e41546e2d0a7a3c802e4e6b3b430c4
philipfleischer <fleischer1@live.no>
2025-10-21 15:04:09 +0200

    Generated the doxyfile. Changed the directory mapping to be more readable and convenient (src, include, etc, folders). Changed the makefile so that the paths would work.

---
+++ docs/html/annotated_dup.js
@@ -0,0 +1,8 @@
+var annotated_dup =
+[
+    [ "ArrayList", "class_array_list.html", "class_array_list" ],
+    [ "ArrayListNode", "struct_array_list_node.html", "struct_array_list_node" ],
+    [ "LinkedArrayList", "class_linked_array_list.html", "class_linked_array_list" ],
+    [ "LinkedList", "class_linked_list.html", "class_linked_list" ],
+    [ "Node", "struct_node.html", "struct_node" ]
+];
